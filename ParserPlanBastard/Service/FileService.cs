2a73f5eed53209b9d4887fbdd98b6ab95e85dec3
Gsgshn <100374838+Gsgshn@users.noreply.github.com>
2023-05-31 21:06:29 +0300

    Revert "Revert "not complete authorization""
    This reverts commit b1ec15e27b514a42ada33054c886cf299ba82b8d.

---
+++ ParserPlanBastard/Service/FileService.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using XSystem.Security.Cryptography;
+
+namespace ParserPlanBastard.Service
+{
+    public class FileService : IFileService
+    {
+        public string ByteArrayToString(byte[] arrInput)
+        {
+            int i;
+            StringBuilder sOutput = new StringBuilder(arrInput.Length);
+            for (i = 0; i < arrInput.Length - 1; i++)
+            {
+                sOutput.Append(arrInput[i].ToString("X2"));
+            }
+            return sOutput.ToString();
+        }
+
+        public bool CheckHash(byte[] firstHash, byte[] secondHash)
+        {
+            bool bEqual = false;
+            if (secondHash.Length == firstHash.Length)
+            {
+                int i = 0;
+                while ((i < secondHash.Length) && (secondHash[i] == firstHash[i]))
+                {
+                    i += 1;
+                }
+                if (i == secondHash.Length)
+                {
+                    return bEqual = true;
+                }
+                else
+                    return bEqual = false;
+            }
+            return bEqual = false;
+        }
+
+        public byte[] ComputeAHash(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public byte[] ComputeAHash(string file)
+        //{
+        //    byte[] tmpSource;
+        //    byte[] tmpNewHash;
+
+        //    tmpSource = Encoding.ASCII.GetBytes(file);
+
+        //    tmpNewHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+
+        //    return tmpNewHash;
+        //}
+    }
+}
---
+++ ParserPlanBastard/Service/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace ParserPlanBastard.Service
+{
+    public interface IFileService
+    {
+        public byte[] ComputeAHash(string file);
+        public bool CheckHash(byte[] firstHash, byte[] secondHash);
+        public string ByteArrayToString(byte[] arrInput);
+    }
+}
